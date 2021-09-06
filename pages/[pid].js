d4568a35076b14489d96156ae94c54bae603a087
Eder <chlrobert93@gmail.com>
2021-09-06 17:46:50 -0500

    Trabajar con parámeteros dinámicos

---
+++ pages/[pid].js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+
+function ProductDetailpage(){
+
+    return(
+        <Fragment>
+            <h1>TITLE</h1>
+            <p>DESCRIPTION</p>
+        </Fragment>
+    );
+}
+
+export default ProductDetailpage;
