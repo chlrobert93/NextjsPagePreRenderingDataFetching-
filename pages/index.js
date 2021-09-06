d39ac1a35479fd04bfb7876d0881eb1f7bb6b6bd
Eder <chlrobert93@gmail.com>
2021-09-06 17:25:18 -0500

    Unamirada más cercana  a getStaticProps y las opciones de configuración

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 //Módulo para la contrucción de caminos
-import path from 'path';
+import path from "path";
 //Módulo del sistema de archivos de node JS
-import fs from 'fs/promises';
+import fs from "fs/promises";
 
 function HomePage(props) {
   const { products } = props;
-  console.log(products);
+  console.log(products.length);
 
   return (
     <ul>
@@ -17,18 +17,35 @@ function HomePage(props) {
 }
 
 //Lo que hace esta función es preparar los props para su compoenente
-//Se ejecuta primero y despues el componenente 
+//Se ejecuta primero y despues el componenente
 //Ejecutar cualquier código  nunca sera visible del lado del cliente
-export async function getStaticProps() {
-  console.log('Re Generating..')
-  //Definir la ruta 
+export async function getStaticProps(context) {
+  console.log("Re Generating..");
+  //Definir la ruta
   //process está disponible globalmente en el nodo JS y podemos ejecutar cwd
- const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
- const jsonData = await fs.readFile(filePath);
- //JSON es un objecto diponible globalmente
- //Lo convierte en JavaScrit normal
- const data = JSON.parse(jsonData);
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  //JSON es un objecto diponible globalmente
+  //Lo convierte en JavaScrit normal
+  const data = JSON.parse(jsonData);
+  
+  console.log(!data);
+    //Redireccionamiento
+    if(!data){
+  
+      return{
+        redirect:{
+          destination: '/no-data'
+        }
+      }
+    }
 
+
+
+  //Comprobar si e objecto esta vacio
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
 
 
   return {
@@ -37,7 +54,8 @@ export async function getStaticProps() {
       products: data.products,
     },
     //Deve volver a generarse
-    revalidate: 10
+    revalidate: 10,
+    //Redireccionamiento
   };
 }
 
