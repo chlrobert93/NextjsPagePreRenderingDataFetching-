b9583bbaf7009456c1df952cb3fc10404241986d
Eder <chlrobert93@gmail.com>
2021-09-06 12:31:11 -0500

    Ejecución de código del lado del servidor y uso del sistema de archivos

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+//Módulo para la contrucción de caminos
+import path from 'path';
+//Módulo del sistema de archivos de node JS
+import fs from 'fs/promises';
+
 function HomePage(props) {
   const { products } = props;
-  console.table(products);
+  console.log(products);
 
   return (
     <ul>
@@ -15,9 +20,20 @@ function HomePage(props) {
 //Se ejecuta primero y despues el componenente 
 //Ejecutar cualquier código  nunca sera visible del lado del cliente
 export async function getStaticProps() {
+  //Definir la ruta 
+  //process está disponible globalmente en el nodo JS y podemos ejecutar cwd
+ const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+ const jsonData = await fs.readFile(filePath);
+ //JSON es un objecto diponible globalmente
+ //Lo convierte en JavaScrit normal
+ const data = JSON.parse(jsonData);
+
+
+
   return {
     props: {
-      products: [{ id: "1", title: "Product  1" }],
+      //products: [{ id: "1", title: "Product  1" }],
+      products: data.products,
     },
   };
 }
