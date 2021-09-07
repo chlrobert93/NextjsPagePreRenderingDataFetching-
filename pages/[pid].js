53719a6eadc92b44844d61f7a92e61706d6f0f95
Eder <chlrobert93@gmail.com>
2021-09-07 15:11:16 -0500

    Carga de rutas de forma dinámica

--- pages/[pid].js
+++ pages/[pid].js
@@ -20,16 +20,22 @@ function ProductDetailpage(props) {
   );
 }
 
+async function getData() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data;
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
 
   //Obtener id de producto
   //Nombre del mismo archivo pid
   const productId = params.pid;
-
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
+  //Obtener datos de la función
+  const data = await getData();
   //filtrar
   const product = data.products.find((product) => product.id === productId);
 
@@ -42,9 +48,14 @@ export async function getStaticProps(context) {
 
 //El objetivo de esta función es decirle a Nextjs que instancias de está página dinámica debe ser generado
 export async function getStaticPaths() {
+  const data = await getData();
+  const ids = data.products.map((product) => product.id);
+  const params = ids.map((id) => ({ params: { pod: id } }));
+
   return {
-    paths: [{ params: { pid: "p1" } }],
-    fallback: true,
+    //paths: [{ params: { pid: "p1" } }],
+    path: params,
+    fallback: false,
     //Esperará para que esta página esté completamente en el servidor
     // fallback: 'blocking'
   };
