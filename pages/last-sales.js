429e4eac1290cf7a0150bc08858d12cbb3f1a7f8
Eder <chlrobert93@gmail.com>
2021-09-09 15:31:21 -0500

    Uso de hooks NextJS useSWR

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,11 +1,34 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
-function LastSalesPage() {
-  const [sales, setSales] = useState();
-
+function LastSalesPage(props) {
+  console.log(props.sales);
+  //props.sales del lado del servidor
+  const [sales, setSales] = useState(props.sales);
   //loading mientras esperamos que lleguen esos resultados
-  const [isLoading, setIsLoading] = useState(false);
+  //const [isLoading, setIsLoading] = useState(false);
 
+  const { data, error } = useSWR(
+    "https://nextjs-course-4f76d-default-rtdb.firebaseio.com/sales.json"
+  );
+
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  /*
   useEffect(() => {
     //Loading al principio
     setIsLoading(true);
@@ -33,27 +56,54 @@ function LastSalesPage() {
         setIsLoading(false);
       });
   }, []);
+*/
+  // if (isLoading) {
+  // return <p>Loading...</p>;
+  // }
 
-  if (isLoading) {
-    return <p>Loading...</p>;
+  if (error) {
+    return <p>Failed to load</p>;
   }
 
-  console.log(sales);
-  if (!sales) {
-    return <p>No data yest</p>;
+  if (!data && !sales) {
+    return <p>Loading....</p>;
   }
+
+  //if (!sales) {
+  //return <p>Loading....</p>;
+  //}
+
   //2
   return (
-   
     <ul>
-      {sales.map(sale => (
+      {sales.map((sale) => (
         <li key={sale.id}>
           {sale.username} - {sale.volume}
         </li>
       ))}
     </ul>
- 
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://nextjs-course-4f76d-default-rtdb.firebaseio.com/sales.json"
+  );
+  //.then((response) => response.json())
+  const data = await response.json();
+  //.then((data) => {
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+
+  return { props: { sales: transformedSales } };
+  //  });
 }
 
 export default LastSalesPage;
